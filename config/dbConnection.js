68b0c4a6b82c21b7ff2f441f9170862f697611c4
Lukas Wessels <Lukas25@live.no>
2024-01-06 18:33:00 +0100

    small console update

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log("Databse connected successfully",
+        console.log("DATABASE CONNECTED:",
         connect.connection.host,
         connect.connection.name);
     } catch (err) {
