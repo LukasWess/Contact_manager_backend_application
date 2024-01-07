916f6505dda4a85a8ddb4d49043bf155608062f7
Lukas Wessels <Lukas25@live.no>
2024-01-07 14:49:24 +0100

    db connection update

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log("DATABASE CONNECTED:",
-        connect.connection.host,
-        connect.connection.name);
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    }
-}
+  try {
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+    console.log(
+      "Mongodb Connected:",
+      connect.connection.host,
+      connect.connection.name
+    );
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
